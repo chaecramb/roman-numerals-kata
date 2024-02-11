8202cd772b2eb1bd3c8bb90662ee1c3c3ae13eae
Chae Cramb <chaecramb@gmail.com>
2024-02-11 20:18:04 +0000

    feat: pass test for all single numerals

--- src/roman.ts
+++ src/roman.ts
@@ -1,3 +1,13 @@
-export function romanConverter(number: number) {
-  return "I";
+export function romanConverter(hinduArabicNumeral: number) {
+  const numeralMap: Record<number, string> = {
+    1: "I",
+    5: "V",
+    10: "X",
+    50: "L",
+    100: "C",
+    500: "D",
+    1000: "M",
+  };
+
+  return numeralMap[hinduArabicNumeral];
 }
