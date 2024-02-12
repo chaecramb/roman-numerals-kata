d7507a57e8c26568184cc1a793c9e8eb19ff5679
Chae Cramb <chaecramb@gmail.com>
2024-02-12 16:06:49 +0000

    refactor: remove logging

--- src/roman.ts
+++ src/roman.ts
@@ -23,28 +23,20 @@ export function romanConverter(
   const largestRomanValueNotExceedingArabic = Math.max(
     ...romanValues.filter((number) => number <= hinduArabicNumeral)
   );
-  console.log(
-    `largestRomanValueNotExceedingArabic: ${largestRomanValueNotExceedingArabic}`
-  );
 
   const integerQuotient = Math.floor(
     hinduArabicNumeral / largestRomanValueNotExceedingArabic
   );
-  console.log(`integerQuotient: ${integerQuotient}`);
 
   const remainder =
     hinduArabicNumeral - integerQuotient * largestRomanValueNotExceedingArabic;
-  console.log(`remainder: ${remainder}`);
 
   romanNumeral +=
     numeralMap[largestRomanValueNotExceedingArabic].repeat(integerQuotient);
-  console.log(`romanNumeral: ${romanNumeral}`);
 
   if (remainder === 0) {
-    console.log(`Returning Roman Numeral: ${romanNumeral}`);
     return romanNumeral;
   } else {
-    console.log(`Calling: romanConverter(${remainder}, ${romanNumeral})`);
     return romanConverter(remainder, romanNumeral);
   }
 }
